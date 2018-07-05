271ee0d306d95aa5872af611afef6678f92ba9df
Ernesto Mendoza <cacho_mexico_93@hotmail.com>
2018-07-05 13:42:03 -0500

    Agregar archivos de proyecto.

---
+++ DatatableCRUD/Models/Extended/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DatatableCRUD.Models
+{
+
+    [MetadataType(typeof(EmployeeMetadata))]
+    public partial class Employee
+    {
+        
+    }
+
+    public class EmployeeMetadata
+    {
+        [Required(ErrorMessage = "Es necesario llenar este campo")]
+        [DisplayName("Nombre:")]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Es necesario llenar este campo")]
+        [DisplayName("Apellido Paterno:")]
+        [StringLength(60)]
+        public string Apellido_Paterno { get; set; }
+
+        [Required(ErrorMessage = "Es necesario llenar este campo")]
+        [DisplayName("Apellido Materno:")]
+        [StringLength(60)]
+        public string Apellido_Materno { get; set; }
+
+        [Required(ErrorMessage = "Es necesario llenar este campo")]
+        [DisplayName("CURP:")]
+        [RegularExpression("[0-9a-zA-Z]+$", ErrorMessage = "Solo se permiten letras y números")]
+        [StringLength(18, MinimumLength = 18, ErrorMessage = "Formato incorrecto")]
+        public string CURP { get; set; }
+    }
+}
